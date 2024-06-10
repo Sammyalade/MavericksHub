d0d9390ef6ac9fb23a8586bed887ffea27c6e49a
Sammyalade <olufemialadeyelu@gmail.com>
2024-06-10 11:29:39 +0100

    MavericksHub

---
+++ src/main/java/com/maverickstube/maverickshub/config/CloudConfig.java
@@ -0,0 +1,29 @@
+package com.maverickstube.maverickshub.config;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+@Configuration
+public class CloudConfig {
+
+    @Value("${cloud.api.name}")
+    private String apiName;
+    @Value("${cloud.api.key}")
+    private String apiKey;
+    @Value("${cloud.api.secret}")
+    private String apiSecret;
+
+    @Bean
+    public Cloudinary cloudinary() {
+        Map<?, ?> map = ObjectUtils.asMap(
+                "cloud_name", apiName,
+                "api_key", apiKey,
+                "api_secret", apiSecret
+        );
+        return new Cloudinary(map);
+    }
+}
